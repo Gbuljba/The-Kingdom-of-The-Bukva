a6e26ab665b813554e422c209810b06c99c21bde
Mislav Juric <shad0w000@hotmail.com>
2012-07-01 16:50:16 +0200

    Header tipkovnice.

---
+++ tipkovnica.h
@@ -0,0 +1,53 @@
+#include <SFML/Window/Keyboard.hpp>
+
+class kretanje
+{
+   public:
+   bool unosNaTipkovnici()
+   {
+	   if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+       {
+          
+       }
+	   else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	   {
+
+	   }
+	   else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	   {
+
+	   }
+	   else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+	   {
+
+	   }
+	   else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+	   {
+
+	   }
+	   else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	   {
+
+	   }
+	   else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+	   {
+
+	   }
+	   else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+	   {
+
+	   }
+	   else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+	   {
+
+	   }
+	   else if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
+	   {
+
+	   }
+	   else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+	   {
+
+	   }
+   }
+};
